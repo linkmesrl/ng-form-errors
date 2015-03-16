3fd9fab03a2261da6c66fca54342158a2f88ca9f
Giovanni Lela <giovanni.lela@link-me.it>
2015-03-16 15:06:22 +0100

    prtractor config for phantomjs

---
+++ blur/blur.js
@@ -0,0 +1,88 @@
+'use strict';
+
+
+/*
+ *  Questa direttiva, se associata ad un elemento
+ *  gli aggiunge una classe invalid se e un proprietà showErr = true
+ *  se l'elemento è non valido ed ha subito un blur, opure se è stato tentato l'invio
+ *  del form e l'elemento non è ancora valido
+ *
+ *
+ *
+ *
+ *
+ */
+
+
+
+angular.module('blurModule', [])
+    .directive('blur', [
+        function() {
+
+            var INVALID_CLASS   = 'invalid';
+            var ERROR_REQUIRED  = 'Campo obbligatorio';
+
+            return {
+                restrict: 'A',
+                require : ['^form', 'ngModel'],
+
+                link: function postLink(scope, element, attrs, ctrls) {
+
+                    var ctrl = ctrls[1];
+                    var form = ctrls[0];
+
+                    if(form.$$parentForm && form.$$parentForm.$name){
+
+                        form = form.$$parentForm;
+                    }
+
+                    ctrl.$showErr = false;
+
+                    scope.$errorMessage = ERROR_REQUIRED;
+
+
+
+                    scope.$watch(function(){return form.$submitted;}, function(submitted){
+
+                        if(submitted=== true && ctrl.$invalid === true){
+
+                            element.addClass(INVALID_CLASS);
+                            ctrl.$showErr = true;
+                        }
+                    });
+
+
+                    //torna subito bello appeno l'utente corregge
+
+                    scope.$watch(function() {return ctrl.$invalid;}, function(invalid){
+
+                        if(!invalid && ctrl.$showErr){
+
+                            element.removeClass(INVALID_CLASS);
+                            ctrl.$showErr = false;
+                        }
+                    });
+
+
+                    element.bind('blur', function() {
+
+                        if(ctrl.$invalid === true){
+
+                            element.addClass(INVALID_CLASS);
+                            scope.$apply(function(){
+                                ctrl.$showErr = true;
+                            });
+                        }
+                        else {
+                            element.removeClass(INVALID_CLASS);
+                            scope.$apply(function(){
+                                ctrl.$showErr = false;
+                            });
+                        }
+                    });
+
+                }
+            };
+        }
+    ]);
+
---
+++ error/errorMessage.js
@@ -0,0 +1,54 @@
+angular.module('errorMessageModule', []).directive('errorMessage', [
+
+    function() {
+
+        'use strict';
+
+        return {
+            restrict: 'E',
+            templateUrl: 'components/error/error.tpl.html',
+            scope: {
+                field:'=field',
+                invalidMessage:'@invalidMessage',
+                errorMsgs: '=errorMsgs',
+                serverError:'=serverError'
+            },
+
+            link: function postLink(scope) {
+
+                var defaultMsg = 'Campo non valido';
+
+                //TODO mettere in inglese?
+
+
+                var errorMessages = {
+
+                    required : 'Campo obbligatorio',
+                    pattern : 'Formato non valido',
+                    number : 'Inserire un numero',
+                    email : 'Formato email non valido',
+                };
+
+                if(scope.errorMsgs){
+
+                    angular.extend(errorMessages, scope.errorMsgs);
+                }
+
+                scope.getErrMsg = function(errType){
+
+                   return scope.invalidMessage || errorMessages[errType] || defaultMsg;
+                };
+
+                if(scope.field)
+                {
+                    scope.$watch('serverError.messages[field.$name]', function(value){
+                        if(value){
+                            scope.field.$showErr = true;
+                        }
+                    });
+                }
+
+            }
+        };
+    }
+]);
