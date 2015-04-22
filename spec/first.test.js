9cd6694cadb828de1e72da3dfdea3c8c9ae75d0c
Matteo Scandolo <matteo.scandolo@link-me.it>
2015-04-22 15:16:48 +0200

    Started to set test Environment

---
+++ spec/first.test.js
@@ -0,0 +1,26 @@
+'use strict';
+
+describe('fallistco', function(){
+    it('should fail forever', function(){
+        expect(false).to.be(true);
+    });
+});
+
+xdescribe('Blur Directive', function () {
+
+  var element, scope;
+  beforeEach(module('blurModule'));
+  beforeEach(inject(function ($compile, $rootScope) {
+      scope = $rootScope.$new();
+      element = $compile('<form><input blur type="number" ng-model="model"></input></form>')(scope);
+    }));
+
+  it('should add a class on blur with invlaid type', function () {
+      scope.model = 'ciao';
+      //scope.$digest();
+      var el = element.find('input');
+      el[0].blur();
+      xexpect(element.find('input').hasClass('invalid')).to.be(true);
+    });
+
+});
